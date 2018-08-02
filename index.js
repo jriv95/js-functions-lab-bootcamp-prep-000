8f96cf2881dcc4654137399c83130e7023335895
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-02 23:02:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // write your code below!
 
-function happyHolidays(){
+function happyHolidays (){
   return "Happy Holidays!"
 }
 
-function happyHolidaysTo(name){
+function happyHolidaysTo (name){
   return "Happy Holidays, ${name}!"
 }
 
-function happyHolidayTo(holiday, name){
+function happyHolidayTo (holiday, name){
   return "Happy ${holiday}, ${name}!"
 }
